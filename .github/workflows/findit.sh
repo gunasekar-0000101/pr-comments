1fabfb3facd77cdef00b1acf2e43d211432efbdd
gunasekar-0000101 <104661507+gunasekar-0000101@users.noreply.github.com>
2022-12-22 21:59:19 +0530

    Create findit.sh

---
+++ .github/workflows/findit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#declare file="rubyout.log"
+#declare regex="\s+build now\s+"
+
+#declare file_content=$( cat "${file}" )
+#if [[ " $file_content " =~ $regex ]] # please note the space before and after the file content
+ #   then
+  #      echo "found"
+   # else
+    #    echo "not found"
+#fi
+
+#exit
+
+if grep -Fxq "build now" rubyout.log
+then
+    # code if found
+    echo "found"
+else
+    # code if not found
+    echo "not found"
+fi
