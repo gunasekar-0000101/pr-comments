55f520d335e3810a5fcd9244ea2917fa5f99e238
gunasekar-0000101 <104661507+gunasekar-0000101@users.noreply.github.com>
2022-12-22 22:20:21 +0530

    Update octokit.rb

--- .github/workflows/octokit.rb
+++ .github/workflows/octokit.rb
@@ -1,9 +1,12 @@
 require 'octokit'
+require 'time'
 
 # !!! DO NOT EVER USE HARD-CODED VALUES IN A REAL APP !!!
 # Instead, set and test environment variables, like below
 client = Octokit::Client.new :access_token => ENV['MY_PERSONAL_TOKEN']
-lookingfor = "2022-12-22 build now"
+#lookingfor = "2022-12-22 build now"
+lookingfor = "build now"
+utctime = Time.now.utc
 client.issue_comments("gunasekar-0000101/pr-comments", 2).each do |comment|
   username = comment[:user][:login]
   post_date = comment[:created_at]
@@ -12,4 +15,5 @@ client.issue_comments("gunasekar-0000101/pr-comments", 2).each do |comment|
   puts content.include?(lookingfor)?
   "#{lookingfor} is present in the comment" :
   "#{lookingfor} is not present in the comment"
+  puts "#{utctime}"
 end
