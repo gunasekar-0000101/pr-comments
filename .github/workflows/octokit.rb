024e51dbf515692700b0db8195011b5188739800
gunasekar-0000101 <104661507+gunasekar-0000101@users.noreply.github.com>
2022-12-23 20:53:50 +0530

    Update octokit.rb

--- .github/workflows/octokit.rb
+++ .github/workflows/octokit.rb
@@ -13,7 +13,7 @@ utctime = Time.now.utc
 #prnum = fileopen.read
 #prnum.close
 #puts prnum
-File.foreach( 'findit.sh' ) do |line|
+File.foreach( './.github/workflows/findit.sh' ) do |line|
   puts "here is your pr number .. how to move this to a variable?"
   puts line
 end
